c91952d3e5cdea286041353f8d25c45ea875cc42
Kyle Govinder <govinder.kyleandrew@gmail.com>
2024-10-06 14:44:49 +0200

    Finish Project 3

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require("body-parser")
+const dns = require("dns")
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -18,6 +21,75 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.post("/api/shorturl", async (req, res) => {
+
+  let original_url = req.body.url
+  console.log(original_url)
+  
+  // Extract domain name
+
+  const httpsRegExp = /(https:\/\/)/
+  const queryStringRexExp = /\/\?.+/
+
+  let temp_url = original_url.replace(httpsRegExp, "")
+  temp_url = temp_url.replace(queryStringRexExp, "")
+   
+    dns.lookup(temp_url, (err) => {
+
+      if (err) {
+
+        res.json({ error: 'invalid url' })
+
+      } else {
+
+        // Check if new URL
+
+        let short_url = shortUrlArr.findIndex((item) => {
+          return item === original_url 
+        })
+
+        // Assign short_url to a new URL
+
+        if (short_url === -1) {
+
+          shortUrlArr.push(original_url)
+          short_url = shortUrlArr.length - 1
+
+        } 
+
+        res.json({
+          original_url,
+          short_url
+        })
+
+      }
+      
+    })    
+
+   }
+
+)
+
+app.get("/api/shorturl/:shorturl", (req, res) => {
+
+  // Find corresponding original_url and redirect 
+
+  const short_url = Number(req.params.shorturl)
+
+  if (isNaN(short_url) || short_url < 0 || short_url + 1 > shortUrlArr.length) {
+
+    res.json({ error: "Invalid Input"})
+
+  } else {
+
+    res.redirect(`${shortUrlArr[short_url]}`)
+
+  }
+
+})
+
+const shortUrlArr = []
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
